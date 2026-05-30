e124db75dffcf8e6a176f2ec782a2e157c9e3a99
Naveen-Raj-DRT <naveen.infor1989@gmail.com>
2026-05-30 19:42:38 +0530

    requirements.txt updated

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import find_package,setup
+from setuptools import find_packages,setup
 from typing import List
 
 def get_requirements()->List[str]:
@@ -8,9 +8,18 @@ def get_requirements()->List[str]:
             lines=file.readlines()
             for line in lines:
                 requirement=line.strip()
-                if requirement and requirement!='-e .':
+                if requirement and requirement!= '-e .':
                     requirement_lst.append(requirement)
     except FileNotFoundError:
-        print("requirements.txt" file not found")
+        print("requirements.txt file not found")
 
     return requirement_lst
+##print(get_requirements())
+setup(
+    name="NetworkSecurity",
+    version="0.0.1",
+    author="Naveen Raj",
+    author_email="naveen.infor1989@gmail.com",
+    packages="find_packages()",
+    install_requires=get_requirements()
+)
